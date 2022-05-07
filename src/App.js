9569b0839dcbc938a776d6fbdb5b8aa417abfa6d
Ziggy-Strdst <vadimpolovinko89@gmail.com>
2022-05-07 15:11:51 +0300

    components-finish

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ export default class App extends Component {
 
     return (
       <div className="App">
+        <h1 className="app-title">My Little Monsters</h1>
         <SearchBox
           className="monsters-search-box"
           onChangeHandler={onSearchChange}
